11f24dcfc62ac419b14d0fba3fb43578ea5576f1
Busyairi AR <busyairiar@gmail.com>
2022-04-15 15:28:45 +0700

    added CRUD feature & endpoint for Lot entities

--- controllers/LotController.js
+++ controllers/LotController.js
@@ -9,6 +9,90 @@ class LotController {
       next(error);
     }
   }
+
+  static async fetchLot(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const lot = await Lot.findOne({ where: { id } });
+
+      if (!lot) throw { name: "Not found" };
+
+      res.status(200).json(lot);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async addLot(req, res, next) {
+    try {
+      const { name, description, width, height, size, startingBid, primaryImage, secondImage, thirdImage, fourthImage, artistName } = req.body;
+      const obj = {
+        name,
+        description,
+        width,
+        height,
+        size,
+        startingBid,
+        SellerId: req.user.id,
+        primaryImage,
+        secondImage,
+        thirdImage,
+        fourthImage,
+        artistName,
+      };
+
+      const lot = await Lot.create(obj);
+
+      res.status(200).json(lot);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async updateLotById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const { name, description, width, height, size, startingBid, primaryImage, secondImage, thirdImage, fourthImage, artistName } = req.body;
+
+      const lot = await Lot.findByPk(id);
+      if (!lot) throw { message: "Not found" };
+
+      const obj = {
+        name,
+        description,
+        width,
+        height,
+        size,
+        startingBid,
+        primaryImage,
+        secondImage,
+        thirdImage,
+        fourthImage,
+        artistName,
+      };
+
+      await Lot.update(obj, { where: { id } });
+      res.status(200).json({ message: "Update success" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteLotById(req, res, next) {
+    try {
+      const { id } = req.params;
+
+      const lot = await Lot.findByPk(id);
+      if (!lot) throw { name: "Not found" };
+
+      await Lot.destroy({ where: { id } });
+
+      res.status(200).json({ message: "Delete success" });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = LotController;
