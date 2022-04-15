b708c5f8b120d3e3c2fdebbc340a3dccd1e437e6
Busyairi AR <busyairiar@gmail.com>
2022-04-15 15:05:52 +0700

    added authN

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const { verifyToken } = require("../helpers/jwt");
+const { User } = require("../models/index");
+
+const authN = async (req, res, next) => {
+  try {
+    const { access_token } = req.headers;
+    if (!access_token) throw { name: "JsonWebTokenError" };
+
+    const payload = verifyToken(access_token);
+
+    if (!payload) throw { name: "JsonWebTokenError" };
+
+    const user = await User.findOne({ where: { id: payload.id, email: payload.email } });
+
+    if (!user) throw { name: "JsonWebTokenError" };
+
+    req.user = {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+    };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authN;
