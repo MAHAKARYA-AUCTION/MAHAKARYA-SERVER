ae38f282c758d151d830ef9ed78fca9296600bd7
Jonathan Sebastian <j.shn50@gmail.com>
2022-04-16 16:35:38 +0700

    callback midtrans

--- controllers/MidtransController.js
+++ controllers/MidtransController.js
@@ -1,5 +1,5 @@
 const midtransClient = require("midtrans-client");
-const { Transaction } = require("../models");
+const { Transaction, User } = require("../models");
 require("dotenv").config();
 // const server_key = process.env.SERVER_MIDTRANS;
 // const client_key = process.env.CLIENT_MIDTRANS;
@@ -12,11 +12,12 @@ class MidtransController {
       serverKey: "SB-Mid-server-1ma6JiKFEciB7nR0KoP5GMH7",
       clientKey: "SB-Mid-client-s6SV0WfrkztKRJyG",
     });
+    let transaction_id = `ORDER-ID-${Math.floor(Date.now() / 10)}`;
     try {
       const { price, UserId } = req.body;
       let parameter = {
         transaction_details: {
-          order_id: `ORDER-ID-${Math.floor(Date.now() / 10)}`,
+          order_id: transaction_id,
           gross_amount: price,
         },
         credit_card: {
@@ -25,7 +26,7 @@ class MidtransController {
       };
       await Transaction.create({
         UserId: UserId,
-        transactionNumber: `TRANS-ID-${Math.floor(Date.now() / 10)}`,
+        transactionNumber: transaction_id,
         price: price,
         status: "pending",
         type: "topup",
@@ -37,12 +38,32 @@ class MidtransController {
       next(err);
     }
   }
-  static async callbackMidtrans (req, res, next) {
+  static async callbackMidtrans(req, res, next) {
     try {
-      console.log({ body: req.body })
+      console.log({ body: req.body });
+      const { transaction_status, order_id } = req.body;
+      if (transaction_status === "settlement") {
+        let transaction = await Transaction.findOne({
+          where: {
+            transactionNumber: order_id,
+          },
+        });
+        if (!transaction) {
+          throw new Error("cant find transaction");
+        }
+        if(transaction.status === 'success') {
+          throw new Error("transaction already settle")
+        }
+        const user = await User.findOne({ where: { id: transaction.UserId } });
+        if (!user) {
+          throw new Error("cant find user");
+        }
+       await user.update({ balance: user.balance + transaction.price })
+       await transaction.update({ status: 'success' })
+      }
     } catch (err) {
-      console.log(err)
-      next(err)
+      console.log(err);
+      next(err);
     }
   }
 }
