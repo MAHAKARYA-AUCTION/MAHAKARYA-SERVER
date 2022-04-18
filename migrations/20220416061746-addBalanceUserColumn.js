fc3c6e87592c9ab6a19ea6cae71ec4dc37fbe05d
Busyairi AR <busyairiar@gmail.com>
2022-04-18 22:10:04 +0700

    hotfix default value balance user

--- migrations/20220416061746-addBalanceUserColumn.js
+++ migrations/20220416061746-addBalanceUserColumn.js
@@ -2,8 +2,8 @@
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Users", "balance", Sequelize.INTEGER);
-    await queryInterface.addColumn("Users", "balanceSpent", Sequelize.INTEGER);
+    await queryInterface.addColumn("Users", "balance", { type: Sequelize.INTEGER, defaultValue: 0 });
+    await queryInterface.addColumn("Users", "balanceSpent", { type: Sequelize.INTEGER, defaultValue: 0 });
   },
 
   async down(queryInterface, Sequelize) {
