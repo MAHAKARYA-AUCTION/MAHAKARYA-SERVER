1310b65a53d692e032305e8b21a5b77236131b42
Busyairi AR <busyairiar@gmail.com>
2022-04-19 09:26:50 +0700

    added test fetch lots & lot by id

--- __tests__/lot.test.js
+++ __tests__/lot.test.js
@@ -1,6 +1,8 @@
 const app = require("../app");
 const request = require("supertest");
 const { Admin, Lot, Collection, User } = require("../models");
+// const lots = require("../data/lots.json");
+const collections = require("../data/collections.json");
 
 beforeAll(async () => {
   await Admin.create({
@@ -9,78 +11,33 @@ beforeAll(async () => {
     password: "12345",
   });
 
-  await User.bulkCreate(
-    {
-      username: "usertestlot1",
-      email: "usertestlot1@email.com",
-      password: "password",
-      ktp: "1111111222111111",
-      phoneNumber: "08123456789",
-      address: "usertestlot1 address",
-      role: "seller",
-    },
-    {
-      username: "usertestlot2",
-      email: "usertestlot2@email.com",
-      password: "password",
-      ktp: "2222222111222222",
-      phoneNumber: "08123456789",
-      address: "usertestlot2 address",
-      role: "seller",
-    }
-  );
+  await User.create({
+    username: "usertestlot1",
+    email: "usertestlot1@email.com",
+    password: "password",
+    ktp: "1111111222111111",
+    phoneNumber: "08123456789",
+    address: "usertestlot1 address",
+    role: "seller",
+  });
 
-  await Collection.bulkCreate(
-    {
-      name: "Realism",
-      imgUrl: "https://res.cloudinary.com/mahakarya/image/upload/v1650263195/paintings/collection-1_z7bs82.jpg",
-      description: "Realism in the arts is generally the attempt to represent subject matter truthfully, without artificiality and avoiding speculative fiction and supernatural elements. The term is often used interchangeably with naturalism, even though these terms are not synonymous.",
-      startDate: "2022-04-15T09:00:00+0700",
-      endDate: "2022-04-23T21:00:00+0700",
-      galleryName: "goliath-gallery",
-      AdminId: 1,
-    },
-    {
-      name: "Surrealism",
-      imgUrl: "https://res.cloudinary.com/mahakarya/image/upload/v1650263195/paintings/collection-2_m4oqre.jpg",
-      description: "Surrealism is a cultural movement that developed in Europe in the aftermath of World War I in which artists depicted unnerving, illogical scenes and developed techniques to allow the unconscious mind to express itself.",
-      startDate: "2022-05-01T09:00:00+0700",
-      endDate: "2022-05-15T21:00:00+0700",
-      galleryName: "david-gallery",
-      AdminId: 1,
-    }
-  );
+  await Collection.bulkCreate(collections);
 
-  await Lot.bulkCreate(
-    {
-      name: "La familia de Carlos IV",
-      description: "Aparecen Goya, los infantes Carlos María Isidro, Francisco de Paula, Carlota Joaquina, María Josefa, María Luisa, Gabriel Antonio, Carlos Luis. También se observa a Luis de Etruria, esposo de María Luisa. Ésta carga en brazos a su hijo Carlos Luis. Otras figuras son los reyes Carlos IV y María Luisa de Parma, el príncipe Fernando y su esposa María Antonieta.",
-      width: 336,
-      height: 280,
-      startingBid: 10000000,
-      SellerId: 1,
-      CollectionId: 1,
-      primaryImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263196/paintings/1-1-1_f8ueab.jpg",
-      secondImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263199/paintings/1-1-2_z4yy3a.jpg",
-      thirdImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263185/paintings/1-1-4_bpwjsz.jpg",
-      artistName: "Francisco de Goya",
-      lotNumber: 1,
-    },
-    {
-      name: "Butcher's Shop",
-      description: "Annibale Carracci - Butcher's Shop (1560-1609)",
-      width: 271,
-      height: 190,
-      startingBid: 15000000,
-      SellerId: 2,
-      CollectionId: 2,
-      primaryImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263186/paintings/1-2-1_pavtui.jpg",
-      secondImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263186/paintings/1-2-2_jax2g6.jpg",
-      thirdImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263185/paintings/1-2-3_mfc4sa.jpg",
-      artistName: "Annibale Carracci",
-      lotNumber: 1,
-    }
-  );
+  await Lot.create({
+    name: "La familia de Carlos IV",
+    description: "Aparecen Goya, los infantes Carlos María Isidro, Francisco de Paula, Carlota Joaquina, María Josefa, María Luisa, Gabriel Antonio, Carlos Luis. También se observa a Luis de Etruria, esposo de María Luisa. Ésta carga en brazos a su hijo Carlos Luis. Otras figuras son los reyes Carlos IV y María Luisa de Parma, el príncipe Fernando y su esposa María Antonieta.",
+    width: 336,
+    height: 280,
+    size: "336 x 280 cm",
+    startingBid: 10000000,
+    SellerId: 1,
+    CollectionId: 1,
+    primaryImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263196/paintings/1-1-1_f8ueab.jpg",
+    secondImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263199/paintings/1-1-2_z4yy3a.jpg",
+    thirdImage: "https://res.cloudinary.com/mahakarya/image/upload/v1650263185/paintings/1-1-4_bpwjsz.jpg",
+    artistName: "Francisco de Goya",
+    lotNumber: 1,
+  });
 });
 
 afterAll(async () => {
@@ -99,14 +56,48 @@ afterAll(async () => {
     cascade: true,
     restartIdentity: true,
   });
+  await User.destroy({
+    truncate: true,
+    cascade: true,
+    restartIdentity: true,
+  });
 });
 
 describe("GET /lots - SUCCESS TEST FETCH ALL LOTS", () => {
   it("should return with status 200", async () => {
     const res = await request(app).get("/lots");
 
-    console.log(res.body);
     expect(res.status).toBe(200);
-    expect(res.body).toHaveProperty("data", expect.any(Array));
+    expect(res.body).toEqual(expect.arrayContaining([expect.any(Object)]));
+  });
+});
+
+describe("GET /lots - SUCCESS TEST FETCH LOT BY ID", () => {
+  it("should return with status 200", async () => {
+    const res = await request(app).get("/lots/1");
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("id", expect.any(Number));
+    expect(res.body).toHaveProperty("name", expect.any(String));
+    expect(res.body).toHaveProperty("description", expect.any(String));
+    expect(res.body).toHaveProperty("width", expect.any(Number));
+    expect(res.body).toHaveProperty("height", expect.any(Number));
+    expect(res.body).toHaveProperty("size", expect.any(String));
+    expect(res.body).toHaveProperty("startingBid", expect.any(Number));
+    expect(res.body).toHaveProperty("primaryImage", expect.any(String));
+    expect(res.body).toHaveProperty("artistName", expect.any(String));
+    expect(res.body).toHaveProperty("SellerId", expect.any(Number));
+    expect(res.body).toHaveProperty("User", expect.any(Object));
+    expect(res.body).toHaveProperty("CollectionId", expect.any(Number));
+    expect(res.body).toHaveProperty("Collection", expect.any(Object));
+  });
+});
+
+describe("GET /lots - FAILED TEST FETCH LOT BY ID LOT NOT FOUND", () => {
+  it("should return with status 200", async () => {
+    const res = await request(app).get("/lots/10");
+
+    expect(res.status).toBe(404);
+    expect(res.body).toHaveProperty("message", "Data not found");
   });
 });
